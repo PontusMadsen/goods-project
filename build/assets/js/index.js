ff060375f2ac41b746d7249c6aacf637b512e0d6
Pontus Madsen <pontus@reizoko.jp>
2024-04-29 15:48:35 +0900

    Trying to commit build forlder to git

---
+++ build/assets/js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+	/**
+  * Remove preload
+  */
+	document.getElementsByTagName("BODY")[0].classList.remove('preload');
+
+	/**
+  * IE support for object-fit
+  */
+	objectFitImages();
+});
+//# sourceMappingURL=index.js.map
